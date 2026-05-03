0623cdd7cc9e0803f25eb73ccbb4f15df706dea4
Baoan Du <baoan.du.xj@renesas.com>
2026-05-03 01:29:32 +0000

    fix: update hand config paths to use rzv_demo_rps directory
    Signed-off-by: Baoan Du <baoan.du.xj@renesas.com>

--- launch/demo_inspire_rh56_hand_rps.launch.py
+++ launch/demo_inspire_rh56_hand_rps.launch.py
@@ -62,12 +62,10 @@ def launch_setup(context, *args, **kwargs):
     foxglove_keypoint_pkg_dir = get_package_share_directory(
         "foxglove_keypoint_publisher"
     )
-    rzv_demo_dexhand_dir = get_package_share_directory("rzv_demo_dexhand")
+    rzv_demo_rps_dir = get_package_share_directory("rzv_demo_rps")
 
     # Shared hand config path used by both interpreter nodes
-    hand_config_path = os.path.join(
-        rzv_demo_dexhand_dir, "config/hand/inspire_rh56.yaml"
-    )
+    hand_config_path = os.path.join(rzv_demo_rps_dir, "config/hand/inspire_rh56.yaml")
 
     # Accumulate all nodes/nodes to return
     nodes = []
